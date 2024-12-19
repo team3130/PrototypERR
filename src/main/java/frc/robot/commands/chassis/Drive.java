7e3e0a276ec318fe84f98aeaf91b398ce644617c
Coriander <145623681+CorianderSeed@users.noreply.github.com>
2024-12-18 19:27:05 -0600

    wrote more stuff in drive

--- src/main/java/frc/robot/commands/chassis/Drive.java
+++ src/main/java/frc/robot/commands/chassis/Drive.java
@@ -1,6 +1,23 @@
 package frc.robot.commands.chassis;
 
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 public class Drive extends Command {
     private final Chassis chassis;
+    private final PS5Controller controller;
+
+    public Drive(Chassis chassis, PS5Controller ps5Controller){
+        this.chassis = chassis;
+        this.controller = ps5Controller;
+
+        m_requirements.add(chassis);
+    }
+    public void initialize() {
+
+    }
+
+    public void execute(){
+        chassis.drive(x, y, theta); //uses either driving or targeting inputs for theta
+    }
+
 }
