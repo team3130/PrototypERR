e7a47892aa855e3b905304307fe0b5fffe017b74
Coriander <145623681+CorianderSeed@users.noreply.github.com>
2025-01-22 20:08:28 -0600

    Fixed typo in ExtendClimber

--- src/main/java/frc/robot/commands/ExtendClimber.java
+++ src/main/java/frc/robot/commands/ExtendClimber.java
@@ -37,6 +37,6 @@ public class ExtendClimber extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return climber.isExteneded();
+    return climber.isExtended();
   }
 }
